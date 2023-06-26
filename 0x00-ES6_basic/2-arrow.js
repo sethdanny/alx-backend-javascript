58e0b2b883c847b390ed5f56617d24f12759a87c
sethdanny <naddulidaniel1994@gmail.com>
2023-06-27 00:52:21 +0300

    arrow function

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,8 +1,7 @@
 export default function getNeighborhoodsList () {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-  const addNeighborhood = (newNeighborhood) => {
+  this.addNeighborhood = (newNeighborhood) => {
     this.sanFranciscoNeighborhoods.push(newNeighborhood);
     return this.sanFranciscoNeighborhoods;
   };
-  return { addNeighborhood };
 }
