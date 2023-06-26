f38c8f6c41c88daeb68fbfbf3d92fb52af01369a
sethdanny <naddulidaniel1994@gmail.com>
2023-06-26 17:30:58 +0300

    fixed all semistandard errors

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,8 @@
+export default function getNeighborhoodsList () {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+  const addNeighborhood = (newNeighborhood) => {
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
+  };
+  return { addNeighborhood };
+}
