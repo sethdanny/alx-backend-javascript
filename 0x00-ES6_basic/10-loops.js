32435fd3297c74ccffb9a8b051964653365c19af
sethdanny <naddulidaniel1994@gmail.com>
2023-06-28 00:49:56 +0300

    added task

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,10 +1,9 @@
-export default function appendToEachArrayValue(array, appendString) {
-    const result = [];
-  
-    for (let value of array) {
-      result.push(appendString + value);
-    }
-  
-    return result;
+export default function appendToEachArrayValue (array, appendString) {
+  const result = []
+
+  for (const value of array) {
+    result.push(appendString + value)
   }
-  
+
+  return result
+}
