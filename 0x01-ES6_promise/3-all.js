73feb8ec770a770b64296f1487a1799e27461cf2
sethdanny <naddulidaniel1994@gmail.com>
2023-06-30 02:49:22 +0300

    return a promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,19 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then((responses) => {
-      const [photoResponse, userResponse] = responses;
-      console.log(photoResponse.body, userResponse.firstName, userResponse.lastName);
-    })
-    .catch(() => {
-      console.log('Signup system offline');
-    });
+  let firstName;
+  let lastName;
+  let body;
+
+  const photo = uploadPhoto();
+  const user = createUser();
+
+  return Promise.all([photo, user]).then((data) => {
+    body = data[0].body;
+    firstName = data[1].firstName;
+    lastName = data[1].lastName;
+    console.log(`${body} ${firstName} ${lastName}`);
+  }).catch(() => {
+    console.log('Signup system offline');
+  });
 }
