05ab4428f8f91be2301754bf28311ae2e662f354
sethdanny <naddulidaniel1994@gmail.com>
2023-06-30 01:34:23 +0300

    fixed

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,12 @@
+export default function getFullResponseFromAPI(success) {
+  return new Promise((reslove, reject) => {
+    if (success) {
+      reslove({
+        status: 200,
+        body: Success,
+      });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
