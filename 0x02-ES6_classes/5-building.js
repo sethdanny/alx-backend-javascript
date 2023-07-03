086c3ebb9d7f6d2104edd6f58cb71cf07a9ded6a
sethdanny <naddulidaniel1994@gmail.com>
2023-07-04 02:56:25 +0300

    added brackets

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,14 @@
+export default class Building {
+	constructor(sqft) {
+		if ((new.target !== Building && this.evacuationWarningMessage === undefined) {
+			throw Error('Class extending Building must override evacuationWarningMessage');
+			
+		} else {
+			this._sqft = sqft;
+		}
+	}
+
+	get sqft() {
+		return this._sqft;
+	}
+}
