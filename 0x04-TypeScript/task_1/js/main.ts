39aff567c539d2225813e1d655498640d59ed882
sethdanny <naddulidaniel1994@gmail.com>
2023-07-06 18:02:54 +0300

    created student interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/class-name-casing */
 interface Teacher {
     readonly firstName: string;
     readonly lastName: string;
@@ -6,4 +8,38 @@ interface Teacher {
     location: string;
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     [propName: string]: any;
+}
+
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+function printTeacher(firstName: string, lastName: string): string {
+    return `${firstName[0]}. ${lastName} `;
+}
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+interface StudentInterface {
+    firstName: string;
+    lastName: string;
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+class StudentClass implements StudentInterface {
+    firstName: string;
+    lastName: string;
+    constructor(firstName: string, lastName: string){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    workOnHomework(): string {
+        return `Currently working`;
+    }
+    displayName(): string {
+        return this.firstName;
+    }
 }
