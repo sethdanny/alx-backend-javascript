daf25584e7acd59ac6e2b9889e09f1b4e2b61d97
sethdanny <naddulidaniel1994@gmail.com>
2023-08-22 16:05:42 +0300

    created server in node

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,19 +1,16 @@
 const http = require('http');
 
-const host = 'localhost';
+const hostname = '127.0.0.1';
 const port = 1245;
 
-const requestListener = (request, response) => {
-  console.log(`Method: ${request.method}`);
-  console.log(`Path: ${request.url}`);
-  console.log(`Headers: ${request.headers}`);
-  response.end('Hello Holberton School!');
-};
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
 
-const app = http.createServer(requestListener);
-
-app.listen(port, host, () => {
-  console.log(`Server running at http://${host}:${port}`);
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
 });
 
 module.exports = app;
