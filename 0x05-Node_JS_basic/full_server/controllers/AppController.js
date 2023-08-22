a1165836a72c5e9f78e0486dbda91dcd06eaebf0
sethdanny <naddulidaniel1994@gmail.com>
2023-08-23 00:08:09 +0300

    fixed

---
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -0,0 +1,13 @@
+/**
+ * Contains the miscellaneous route handlers.
+ * @author Nadduli Daniel <https://github.com/sethdanny>
+ */
+
+class AppController {
+  static getHomepage(request, response) {
+    response.status(200).send('Hello Holberton School!');
+  }
+}
+
+module.exports = AppController;
+export default AppController;
