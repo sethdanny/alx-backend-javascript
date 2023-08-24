d351ae11ce5fb993cc2ad9aee3c5ce70d64a98a3
sethdanny <naddulidaniel1994@gmail.com>
2023-08-24 14:26:55 +0300

    used chai expect value

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,129 +1,104 @@
-const assert = require('assert');
-const calculateNumber = require('./1-calcul');
+const { expect } = require('chai');
+const calculateNumber = require('./2-calcul_chai');
 
 describe('calculateNumber', () => {
   it('type == "SUM" - equal positive numbers', () => {
-    const result = calculateNumber('SUM', 2, 2);
-    assert.strictEqual(result, 4);
+    expect(calculateNumber('SUM', 2, 2)).to.equal(4);
   });
 
   it('type == "SUM" - equal positive numbers(alternate)', () => {
-    const result = calculateNumber('SUM', 2.4, 2.6);
-    assert.strictEqual(result, 5);
+    expect(calculateNumber('SUM', 2.4, 2.6)).to.equal(5);
   });
 
   it('type == "SUM" - equal negative numbers', () => {
-    const result = calculateNumber('SUM', -2, -2);
-    assert.strictEqual(result, -4);
+    expect(calculateNumber('SUM', -2, -2)).to.equal(-4);
   });
 
   it('type == "SUM" - equal negative numbers(alternate)', () => {
-    const result = calculateNumber('SUM', -2.4, -2.6);
-    assert.strictEqual(result, -5);
+    expect(calculateNumber('SUM', -2.4, -2.6)).to.equal(-5);
   });
 
   it('type == "SUM" - positive and negative numbers', () => {
-    const result = calculateNumber('SUM', 2, -2);
-    assert.strictEqual(result, 0);
+    expect(calculateNumber('SUM', 2, -2)).to.equal(0);
   });
 
   it('type == "SUM" - negative and negative numbers', () => {
-    const result = calculateNumber('SUM', -2.4, 2.6);
-    assert.strictEqual(result, 1);
+    expect(calculateNumber('SUM', -2.4, 2.6)).to.equal(1);
   });
 
   it('type == "SUBTRACT" - equal positive numbers', () => {
-    const result = calculateNumber('SUBTRACT', 2, 2);
-    assert.strictEqual(result, 0);
+    expect(calculateNumber('SUBTRACT', 2, 2)).to.equal(0);
   });
 
   it('type == "SUBTRACT" - equal positive numbers(alternate)', () => {
-    const result = calculateNumber('SUBTRACT', 2.3, 1.6);
-    assert.strictEqual(result, 0);
+    expect(calculateNumber('SUBTRACT', 2.3, 1.6)).to.equal(0);
   });
 
   it('type == "SUBTRACT" - equal negative numbers', () => {
-    const result = calculateNumber('SUBTRACT', -2, -2);
-    assert.strictEqual(result, 0);
+    expect(calculateNumber('SUBTRACT', -2, -2)).to.equal(0);
   });
 
   it('type == "SUBTRACT" - equal negative numbers(alternate)', () => {
-    const result = calculateNumber('SUBTRACT', -2.3, -1.6);
-    assert.strictEqual(result, 0);
+    expect(calculateNumber('SUBTRACT', -2.3, -1.6)).to.equal(0);
   });
 
   it('type == "SUBTRACT" - positive and negative numbers', () => {
-    const result = calculateNumber('SUBTRACT', 2, -2);
-    assert.strictEqual(result, 4);
+    expect(calculateNumber('SUBTRACT', 2, -2)).to.equal(4);
   });
 
   it('type == "SUBTRACT" - negative and positive numbers', () => {
-    const result = calculateNumber('SUBTRACT', -2.3, 1.6);
-    assert.strictEqual(result, -4);
+    expect(calculateNumber('SUBTRACT', -2.3, 1.6)).to.equal(-4);
   });
 
   it('type === "SUBTRACT" - zero and zero', () => {
-    const result = calculateNumber('SUBTRACT', 0.0, 0.0);
-    assert.strictEqual(result, 0);
+    expect(calculateNumber('SUBTRACT', 0.0, 0.0)).to.equal(0);
   });
 
   it('type ==="DIVIDE" - positive and positive numbers', () => {
-    const result = calculateNumber('DIVIDE', 2, 2);
-    assert.strictEqual(result, 1);
+    expect(calculateNumber('DIVIDE', 2, 2)).to.equal(1);
   });
 
   it('type ==="DIVIDE" - positive and positive numbers(alternate)', () => {
-    const result = calculateNumber('DIVIDE', 2.3, 1.6);
-    assert.strictEqual(result, 1);
+    expect(calculateNumber('DIVIDE', 2.3, 1.6)).to.equal(1);
   });
 
   it('type ==="DIVIDE" - positive and negative numbers', () => {
-    const result = calculateNumber('DIVIDE', 2, -2);
-    assert.strictEqual(result, -1);
+    expect(calculateNumber('DIVIDE', 2, -2)).to.equal(-1);
   });
 
   it('type ==="DIVIDE" - positive and negative numbers(alternate)', () => {
-    const result = calculateNumber('DIVIDE', 2.3, -1.6);
-    assert.strictEqual(result, -1);
+    expect(calculateNumber('DIVIDE', 2.3, -1.6)).to.equal(-1);
   });
 
   it('type ==="DIVIDE" - negative and positive numbers', () => {
-    const result = calculateNumber('DIVIDE', -2, 2);
-    assert.strictEqual(result, -1);
+    expect(calculateNumber('DIVIDE', -2, 2)).to.equal(-1);
   });
 
   it('type ==="DIVIDE" - negative and positive numbers(alternate)', () => {
-    const result = calculateNumber('DIVIDE', -2.3, 1.6);
-    assert.strictEqual(result, -1);
+    expect(calculateNumber('DIVIDE', -2.3, 1.6)).to.equal(-1);
   });
 
   it('type ==="DIVIDE" - negative and negative numbers', () => {
-    const result = calculateNumber('DIVIDE', -2, -2);
-    assert.strictEqual(result, 1);
+    expect(calculateNumber('DIVIDE', -2, -2)).to.equal(1);
   });
 
   it('type ==="DIVIDE" - negative and negative numbers(alternate)', () => {
-    const result = calculateNumber('DIVIDE', -2.3, -1.6);
-    assert.strictEqual(result, 1);
+    expect(calculateNumber('DIVIDE', -2.3, -1.6)).to.equal(1);
   });
 
   it('type ==="DIVIDE" - positive and zero', () => {
-    const result = calculateNumber('DIVIDE', 2, 0);
-    assert.strictEqual(result, 'Error');
+    expect(calculateNumber('DIVIDE', 2, 0)).to.equal('Error');
   });
 
   it('type ==="DIVIDE" - negative and zero', () => {
-    const result = calculateNumber('DIVIDE', -2, 0);
-    assert.strictEqual(result, 'Error');
+    expect(calculateNumber('DIVIDE', -2, 0)).to.equal('Error');
   });
 
   it('type ==="DIVIDE" - zero and zero', () => {
-    const result = calculateNumber('DIVIDE', 0, 0);
-    assert.strictEqual(result, 'Error');
+    expect(calculateNumber('DIVIDE', 0, 0)).to.equal('Error');
   });
 
   it('type ==="DIVIDE" - zero and positive', () => {
-    const result = calculateNumber('DIVIDE', 0, 2);
-    assert.strictEqual(result, 0);
+    expect(calculateNumber('DIVIDE', 0, 2)).to.equal(0);
   });
 });
