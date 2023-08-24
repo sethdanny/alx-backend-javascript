0013c84bc31a36288407c694c62050cee9718243
sethdanny <naddulidaniel1994@gmail.com>
2023-08-24 12:28:44 +0300

    created nodejs test using assert

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -31,4 +31,99 @@ describe('calculateNumber', () => {
     const result = calculateNumber('SUM', -2.4, 2.6);
     assert.strictEqual(result, 1);
   });
+
+  it('type == "SUBTRACT" - equal positive numbers', () => {
+    const result = calculateNumber('SUBTRACT', 2, 2);
+    assert.strictEqual(result, 0);
+  });
+
+  it('type == "SUBTRACT" - equal positive numbers(alternate)', () => {
+    const result = calculateNumber('SUBTRACT', 2.3, 1.6);
+    assert.strictEqual(result, 0);
+  });
+
+  it('type == "SUBTRACT" - equal negative numbers', () => {
+    const result = calculateNumber('SUBTRACT', -2, -2);
+    assert.strictEqual(result, 0);
+  });
+
+  it('type == "SUBTRACT" - equal negative numbers(alternate)', () => {
+    const result = calculateNumber('SUBTRACT', -2.3, -1.6);
+    assert.strictEqual(result, 0);
+  });
+
+  it('type == "SUBTRACT" - positive and negative numbers', () => {
+    const result = calculateNumber('SUBTRACT', 2, -2);
+    assert.strictEqual(result, 4);
+  });
+
+  it('type == "SUBTRACT" - negative and positive numbers', () => {
+    const result = calculateNumber('SUBTRACT', -2.3, 1.6);
+    assert.strictEqual(result, -4);
+  });
+
+  it('type === "SUBTRACT" - zero and zero', () => {
+    const result = calculateNumber('SUBTRACT', 0.0, 0.0);
+    assert.strictEqual(result, 0);
+  });
+
+  it('type ==="DIVIDE" - positive and positive numbers', () => {
+    const result = calculateNumber('DIVIDE', 2, 2);
+    assert.strictEqual(result, 1);
+  });
+
+  it('type ==="DIVIDE" - positive and positive numbers(alternate)', () => {
+    const result = calculateNumber('DIVIDE', 2.3, 1.6);
+    assert.strictEqual(result, 1);
+  });
+
+  it('type ==="DIVIDE" - positive and negative numbers', () => {
+    const result = calculateNumber('DIVIDE', 2, -2);
+    assert.strictEqual(result, -1);
+  });
+
+  it('type ==="DIVIDE" - positive and negative numbers(alternate)', () => {
+    const result = calculateNumber('DIVIDE', 2.3, -1.6);
+    assert.strictEqual(result, -1);
+  });
+
+  it('type ==="DIVIDE" - negative and positive numbers', () => {
+    const result = calculateNumber('DIVIDE', -2, 2);
+    assert.strictEqual(result, -1);
+  });
+
+  it('type ==="DIVIDE" - negative and positive numbers(alternate)', () => {
+    const result = calculateNumber('DIVIDE', -2.3, 1.6);
+    assert.strictEqual(result, -1);
+  });
+
+  it('type ==="DIVIDE" - negative and negative numbers', () => {
+    const result = calculateNumber('DIVIDE', -2, -2);
+    assert.strictEqual(result, 1);
+  });
+
+  it('type ==="DIVIDE" - negative and negative numbers(alternate)', () => {
+    const result = calculateNumber('DIVIDE', -2.3, -1.6);
+    assert.strictEqual(result, 1);
+  });
+
+  it('type ==="DIVIDE" - positive and zero', () => {
+    const result = calculateNumber('DIVIDE', 2, 0);
+    assert.strictEqual(result, 'Error');
+  });
+
+  it('type ==="DIVIDE" - negative and zero', () => {
+    const result = calculateNumber('DIVIDE', -2, 0);
+    assert.strictEqual(result, 'Error');
+  });
+
+  it('type ==="DIVIDE" - zero and zero', () => {
+    const result = calculateNumber('DIVIDE', 0, 0);
+    assert.strictEqual(result, 'Error');
+  });
+
+  it('type ==="DIVIDE" - zero and positive', () => {
+    const result = calculateNumber('DIVIDE', 0, 2);
+    assert.strictEqual(result, 0);
+  });
 });
