13cbbf2dbdc75ccd90bbcead975de33912e1c306
sethdanny <naddulidaniel1994@gmail.com>
2023-08-25 15:07:31 +0300

    created payment api

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,15 @@
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+describe('sendPaymentRequestToApi', () => {
+  it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
+    const bigBrother = sinon.spy(Utils);
+
+    sendPaymentRequestToApi(100, 20);
+    expect(bigBrother.calculateNumber.alwaysCalledWith('SUM', 100, 20)).to.be.true;
+    expect(bigBrother.calculateNumber.callCount).to.be.equal(1);
+    bigBrother.calculateNumber.restore();
+  });
+});
---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,17 @@
+const Utils = {
+  calculateNumber (type, a, b) {
+    if (type === 'SUM') {
+      return Math.round(a) + Math.round(b);
+    } else if (type === 'SUBTRACT') {
+      return Math.round(a) - Math.round(b);
+    } else if (type === 'DIVIDE') {
+      if (Math.round(b) === 0) {
+        return 'Error';
+      }
+      return Math.round(a) / Math.round(b);
+    } else {
+      throw new Error('Unknown type: ' + type);
+    }
+  }
+};
+module.exports = Utils;
